6a54bcef8fe520615cb221d632556d792f5dd057
owen-delahoy <owen.delahoy@calmid.com>
2020-07-13 16:19:53 +0900

    Server deploy script

--- live-server/server/deploy-prod-india.sh
+++ live-server/server/deploy-prod-india.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 494634321140.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t prod-kidsloop-live .
 docker tag prod-kidsloop-live:latest 494634321140.dkr.ecr.ap-south-1.amazonaws.com/prod-kidsloop-live:latest
