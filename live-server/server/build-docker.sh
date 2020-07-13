8d6bc1697df8c36d1c63d703f2c134d27e87cd98
owen-delahoy <owen.delahoy@calmid.com>
2020-07-13 16:16:47 +0900

    Docker build

---
+++ live-server/server/build-docker.sh
@@ -0,0 +1,4 @@
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 494634321140.dkr.ecr.ap-south-1.amazonaws.com
+docker build -t prod-kidsloop-live .
+docker tag prod-kidsloop-live:latest 494634321140.dkr.ecr.ap-south-1.amazonaws.com/prod-kidsloop-live:latest
+docker push 494634321140.dkr.ecr.ap-south-1.amazonaws.com/prod-kidsloop-live:latest
