e5926113a991c063ee5ebef14cc48f053f534bdf
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-10-12 14:23:35 +0900

    feat(new-relic): add typing for newrelic base package. Add new relic apollo server plugin, configures plugin for the apollo server.  Apollo server has no typing information available; add .d.ts file declaring type for plugin, update .tsconfig to declare .d.ts
    KLL-1683

---
+++ types/newrelic.d.ts
@@ -0,0 +1,7 @@
+declare module "@newrelic/apollo-server-plugin" {
+    import type { ApolloServerPlugin } from "apollo-server-plugin-base";
+  
+    let NRApolloPlugin: ApolloServerPlugin;
+  
+    export default NRApolloPlugin;
+}
