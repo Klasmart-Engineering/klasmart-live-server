4cb5cbe05d3cdbfe29d3d55fe8d82173ea420d18
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-10-12 15:26:33 +0900

    fix(new-relic): fix apollo-server-plugin type declaration resolution. update .gitignore with newrelic logfile
    Fix type declaration resolution. Type declaration modules need to be in folders that resolve similarly to the packages themselves. There is special formatting for type declarations that map to packages that are in child folders.  For this case that meant that @newrelic/apollo-server-plugin had to be resolved through newrelic__apollo-server-plugin
    
    KLL-1683

--- types/newrelic__apollo-server-plugin/index.d.ts
+++ types/newrelic__apollo-server-plugin/index.d.ts
@@ -2,6 +2,5 @@ declare module "@newrelic/apollo-server-plugin" {
     import type { ApolloServerPlugin } from "apollo-server-plugin-base";
   
     let NRApolloPlugin: ApolloServerPlugin;
-  
     export default NRApolloPlugin;
 }
