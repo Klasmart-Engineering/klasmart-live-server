5e44b8d481f5b41a3758e05a6b93ce1d710bc9ba
mamur <mamur.abdullaev@calmid.com>
2021-11-26 18:10:22 +0900

    style(.eslintrc.js): no-exlicit-any OFF [ci skip]

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     ],
     rules: {
         "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": ["off"],
         "@typescript-eslint/no-unused-vars": [
             1,
         ],
