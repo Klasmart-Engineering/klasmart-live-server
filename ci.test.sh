807afefe37b851b515720c6e5dd1f039dad64e4e
jean.georges <jean.georges@kidsloop.live>
2022-05-31 14:06:21 +0900

    chore: running tests inside a container

--- ci.test.sh
+++ ci.test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-docker compose -f docker-compose.test.yml up
+docker compose -f docker-compose.test.yml down
+docker compose -f docker-compose.test.yml rm
+docker compose -f docker-compose.test.yml up --abort-on-container-exit --remove-orphans --renew-anon-volumes --timeout 1
+
+docker logs -t kltest-live-server > ci-test.log
 
 # docker sbom redis:alpine
-# docker sbom kl-local-live-server:latest
+# docker sbom kl-local-live-server:latest 
