d5b7204a546f905e5a0741ac45d4a365cd118d24
mamur <mamur.abdullaev@calmid.com>
2022-06-07 11:59:46 +0900

    fix: set fake STUDENT_REPORT_ENDPOINT env variable

--- tests/integration/config/setEnvVars.ts
+++ tests/integration/config/setEnvVars.ts
@@ -1,3 +1,4 @@
 import { getUniqueId } from "../mockData/functions";
 process.env.SESSION_ID = getUniqueId();
 process.env.ATTENDANCE_SERVICE_ENDPOINT="http://localhost:3000/attendance";
+process.env.STUDENT_REPORT_ENDPOINT="http://localhost:3000/v1/student_usage_record/event"
